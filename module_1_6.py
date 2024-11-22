24c39280215b022b44affa69ca85ed58c995f63d
alphadekw <163714728+alphadekw@users.noreply.github.com>
2024-11-22 23:04:30 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,23 @@
+my_dict = {'Vlad': 2007, 'Maks': 2005, 'Anton': 2006}
+print(my_dict)
+print(my_dict['Vlad'])
+print(my_dict.get('Katya'))
+
+my_dict['Vadim'] = 2000
+my_dict['Liza'] = 2001
+print(my_dict)
+
+del my_dict['Maks']
+print(my_dict)
+
+#множество
+
+my_set = {1, 2, 2, 1, 3, 3, 3}
+print(my_set)
+
+(my_set.add(6))
+(my_set.add(7))
+print(my_set)
+
+(my_set.discard(2))
+print(my_set)
