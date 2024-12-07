5dce897931b21fb11ef39ed353a457dc154ca94a
alphadekw <163714728+alphadekw@users.noreply.github.com>
2024-12-08 00:51:40 +0300

    Update2 module_1_6.py

--- module_1_6.py
+++ module_1_6.py
@@ -2,25 +2,17 @@ my_dict = {'Vlad': 2007, 'Maks': 2005, 'Anton': 2006}
 print(my_dict)
 print(my_dict['Vlad'])
 print(my_dict.get('Katya'))
-
 my_dict['Vadim'] = 2000
 my_dict['Liza'] = 2001
-print(my_dict)
-
-del my_dict['Maks']
+print(my_dict.pop('Anton'))
 print(my_dict)
 
 #множество
 
 my_set = {1, 2, 2, 1, 3, 3, 3}
 print(my_set)
-
-(my_set.add(6))
-(my_set.add(7))
+(my_set.update({6, 7}))
+print(my_set)
+(my_set.remove(2))
 print(my_set)
 
-(my_set.discard(2))
-print(my_set)
-
-delete = my_set.pop()
-print(my_set)
